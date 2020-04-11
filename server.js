3e2b70b94ff58aabb51d134e6d3c5a66387f1280
karannegi20 <karan.negi20@gmail.com>
2020-04-11 20:44:09 +0530

    Initial routes creation and test

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
+const mongoose = require('mongoose');
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('API running');
 });
 
+const userRouter = require('./routes/api/users');
+const postsRouter = require('./routes/api/posts');
+const authRouter = require('./routes/api/auth');
+const profileRouter = require('./routes/api/profile');
+
+app.use('/api/users', userRouter);
+app.use('/api/posts', postsRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/profile', profileRouter);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}...`);
 });
